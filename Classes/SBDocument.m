49ee3ed9abcffbc8771ff34631009d1c37bea506
Alice Atlas <alice@atlas.ai>
2014-08-26 03:41:55 -0400

    SBHistoryView

--- Classes/SBDocument.m
+++ Classes/SBDocument.m
@@ -27,7 +27,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #import "SBBookmarkListView.h"
 #import "SBBookmarkView.h"
 #import "SBDownloadsView.h"
-#import "SBHistoryView.h"
 #import "SBInnerView.h"
 #import "SBReportView.h"
 #import "SBSidebar.h"
---
+++ Classes/SBHistoryView.swift
@@ -0,0 +1,381 @@
+let kSBMinFrameSizeWidth: CGFloat = 480
+let kSBMinFrameSizeHeight: CGFloat = 320
+
+
+class SBHistoryView: SBView, NSTextFieldDelegate, NSTableViewDelegate, NSTableViewDataSource {
+	private lazy var iconImageView: NSImageView = {
+        let image: NSImage? = NSImage(named: "History")
+        let iconImageView = NSImageView(frame: self.iconRect)
+        if image != nil {
+            image!.size = iconImageView.frame.size
+            iconImageView.image = image!
+        }
+        return iconImageView
+    }()
+	private lazy var messageLabel: NSTextField = {
+        let messageLabel = NSTextField(frame: self.messageLabelRect)
+        messageLabel.autoresizingMask = .ViewMinXMargin | .ViewMinYMargin
+        messageLabel.editable = false
+        messageLabel.bordered = false
+        messageLabel.drawsBackground = false
+        messageLabel.textColor = NSColor.whiteColor()
+        messageLabel.font = NSFont.boldSystemFontOfSize(16)
+        messageLabel.alignment = .LeftTextAlignment
+        (messageLabel.cell() as NSTextFieldCell).wraps = true
+        return messageLabel
+    }()
+	private lazy var searchField: SBBLKGUISearchField = {
+        let searchField = SBBLKGUISearchField(frame: self.searchFieldRect)
+        searchField.delegate = self
+        searchField.target = self
+        searchField.action = "search:"
+        (searchField.cell() as NSSearchFieldCell).sendsWholeSearchString = true
+        (searchField.cell() as NSSearchFieldCell).sendsSearchStringImmediately = true
+        return searchField
+    }()
+	private lazy var scrollView: SBBLKGUIScrollView = {
+        let scrollView = SBBLKGUIScrollView(frame: self.tableViewRect)
+        scrollView.autoresizingMask = .ViewWidthSizable | .ViewHeightSizable
+        scrollView.autohidesScrollers = true
+        scrollView.hasVerticalScroller = true
+        scrollView.hasHorizontalScroller = false
+        scrollView.autohidesScrollers = true
+        scrollView.backgroundColor = NSColor.blackColor()
+        scrollView.drawsBackground = false
+        scrollView.documentView = self.tableView
+        return scrollView
+    }()
+	private lazy var tableView: NSTableView = {
+        var tableRect = NSZeroRect
+        tableRect.size = self.tableViewRect.size
+        let tableView = NSTableView(frame: tableRect)
+        let iconColumn = NSTableColumn(identifier: kSBImage)
+        let titleColumn = NSTableColumn(identifier: kSBTitle)
+        let urlColumn = NSTableColumn(identifier: kSBURL)
+        let dateColumn = NSTableColumn(identifier: kSBDate)
+        let iconCell = SBIconDataCell()
+        let textCell = NSCell()
+        iconCell.drawsBackground = false
+        iconColumn.width = 22.0
+        iconColumn.dataCell = iconCell
+        iconColumn.editable = false
+        titleColumn.dataCell = textCell
+        titleColumn.width = (tableRect.size.width - 22.0) * 0.3
+        titleColumn.editable = false
+        urlColumn.dataCell = textCell
+        urlColumn.width = (tableRect.size.width - 22.0) * 0.4
+        urlColumn.editable = false
+        dateColumn.dataCell = textCell
+        dateColumn.width = (tableRect.size.width - 22.0) * 0.3
+        dateColumn.editable = false
+        tableView.backgroundColor = NSColor.clearColor()
+        tableView.rowHeight = 20
+        tableView.addTableColumn(iconColumn)
+        tableView.addTableColumn(titleColumn)
+        tableView.addTableColumn(urlColumn)
+        tableView.addTableColumn(dateColumn)
+        tableView.allowsMultipleSelection = true
+        tableView.allowsColumnSelection = false
+        tableView.allowsEmptySelection = true
+        tableView.doubleAction = "tableViewDidDoubleAction:"
+        tableView.columnAutoresizingStyle = .LastColumnOnlyAutoresizingStyle
+        tableView.headerView = nil
+        tableView.cornerView = nil
+        tableView.autoresizingMask = .ViewWidthSizable
+        tableView.setDataSource(self)
+        tableView.setDelegate(self)
+        tableView.focusRingType = .None
+        tableView.doubleAction = "open"
+        return tableView
+    }()
+	private lazy var removeButton: SBBLKGUIButton = {
+        let removeButton = SBBLKGUIButton(frame: self.removeButtonRect)
+        removeButton.title = NSLocalizedString("Remove", comment: "")
+        removeButton.target = self
+        removeButton.action = "remove"
+        removeButton.enabled = false
+        return removeButton
+    }()
+	private lazy var removeAllButton: SBBLKGUIButton = {
+        let removeAllButton = SBBLKGUIButton(frame: self.removeAllButtonRect)
+        removeAllButton.title = NSLocalizedString("Remove All", comment: "")
+        removeAllButton.target = self
+        removeAllButton.action = "removeAll"
+        removeAllButton.enabled = false
+        return removeAllButton
+    }()
+	private lazy var backButton: SBBLKGUIButton = {
+        let backButton = SBBLKGUIButton(frame: self.backButtonRect)
+        backButton.title = NSLocalizedString("Back", comment: "")
+        backButton.target = self
+        backButton.action = "cancel"
+        backButton.keyEquivalent = "\u{1B}"
+        return backButton
+    }()
+    
+    var message: String {
+        get { return messageLabel.stringValue }
+        set(message) { messageLabel.stringValue = message }
+    }
+	var items: [WebHistoryItem]
+    
+    override init(frame: NSRect) {
+        var r = frame
+        r.size.width = max(r.size.width, kSBMinFrameSizeWidth)
+        r.size.height = max(r.size.height, kSBMinFrameSizeWidth)
+        items = SBHistory.sharedHistory.items
+        super.init(frame: r)
+        addSubview(iconImageView)
+        addSubview(messageLabel)
+        addSubview(searchField)
+        addSubview(scrollView)
+        addSubview(removeButton)
+        addSubview(removeAllButton)
+        addSubview(backButton)
+        makeResponderChain()
+        autoresizingMask = .ViewMinXMargin | .ViewMaxXMargin | .ViewMinYMargin | .ViewMaxYMargin
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+    
+    // MARK: Rects
+    
+    let margin = NSMakePoint(36.0, 32.0)
+    let labelWidth: CGFloat = 85.0
+    let buttonHeight: CGFloat = 24.0
+    let buttonMargin: CGFloat = 15.0
+    let searchFieldWidth: CGFloat = 250.0
+    
+    var iconRect: NSRect {
+        var r = NSZeroRect
+        r.size.width = 32.0
+        r.origin.x = labelWidth - r.size.width
+        r.size.height = 32.0
+        r.origin.y = bounds.size.height - margin.y - r.size.height
+        return r
+    }
+    
+    var messageLabelRect: NSRect {
+        var r = NSZeroRect
+        r.origin.x = NSMaxX(iconRect) + 10.0
+        r.size.width = bounds.size.width - r.origin.x - searchFieldWidth - margin.x
+        r.size.height = 20.0
+        r.origin.y = bounds.size.height - margin.y - r.size.height - (iconRect.size.height - r.size.height) / 2
+        return r
+    }
+    
+    var searchFieldRect: NSRect {
+        var r = NSZeroRect
+        r.size.width = searchFieldWidth
+        r.size.height = 20.0
+        r.origin.x = bounds.size.width - r.size.width - margin.x
+        r.origin.y = bounds.size.height - margin.y - r.size.height - (iconRect.size.height - r.size.height) / 2
+        return r
+    }
+    
+    var tableViewRect: NSRect {
+        var r = NSZeroRect
+        r.origin.x = margin.x
+        r.size.width = bounds.size.width - r.origin.x - margin.x
+        r.size.height = bounds.size.height - iconRect.size.height - 10.0 - margin.y * 3 - buttonHeight
+        r.origin.y = margin.y * 2 + buttonHeight
+        return r
+    }
+    
+    var removeButtonRect: NSRect {
+        var r = NSZeroRect
+        r.size.width = 105.0
+        r.size.height = buttonHeight
+        r.origin.y = margin.y
+        r.origin.x = margin.x
+        return r
+    }
+    
+    var removeAllButtonRect: NSRect {
+        var r = NSZeroRect
+        r.size.width = 140.0
+        r.size.height = removeButtonRect.size.height
+        r.origin.y = margin.y
+        r.origin.x = NSMaxX(removeButtonRect) + 10.0
+        return r
+    }
+    
+    var backButtonRect: NSRect {
+        var r = NSZeroRect
+        r.size.width = 105.0
+        r.size.height = buttonHeight
+        r.origin.y = margin.y
+        r.origin.x = bounds.size.width - r.size.width - margin.x
+        return r
+    }
+    
+    func showAllItems() {
+        items = SBHistory.sharedHistory.items
+        tableView.reloadData()
+    }
+    
+    func updateItems() {
+        let allItems = SBHistory.sharedHistory.items
+        let searchFieldText = searchField.stringValue
+        let searchWords = !searchFieldText.isEmpty ? (searchFieldText as NSString).componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) as [String] : []
+        if !searchWords.isEmpty {
+            items = []
+            for item in allItems {
+                var string = ""
+                if item.originalURLString != nil { string += " \(item.originalURLString)" }
+                if item.URLString != nil { string += " \(item.URLString)" }
+                if item.title != nil { string += " \(item.title)" }
+                if !string.isEmpty {
+                    var index = 0
+                    for searchWord in searchWords {
+                        if searchWord.isEmpty || (string as NSString).rangeOfString(searchWord, options: .CaseInsensitiveSearch).location != NSNotFound {
+                            if index == searchWords.count - 1 {
+                                items.append(item)
+                            }
+                        } else {
+                            break
+                        }
+                        index++
+                    }
+                }
+            }
+        } else {
+            items = allItems
+        }
+    }
+    
+    // MARK: DataSource
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        removeAllButton.enabled = !items.isEmpty
+        return items.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn, row rowIndex: Int) -> AnyObject? {
+        let identifier = tableColumn.identifier
+        let item: WebHistoryItem? = (rowIndex < items.count) ? items[rowIndex] : nil
+        switch identifier {
+        case kSBTitle:
+            return item?.title
+        case kSBURL:
+            return item?.URLString
+        case kSBDate:
+            return nil
+        default:
+            break
+        }
+        return nil
+    }
+    
+    func tableView(tableView: NSTableView, willDisplayCell cell: NSCell, forTableColumn tableColumn: NSTableColumn, row rowIndex: Int) {
+        let identifier = tableColumn.identifier
+        let item: WebHistoryItem? = (rowIndex < items.count) ? items[rowIndex] : nil
+        var string: String?
+        switch identifier {
+        case kSBImage:
+            if let image = item?.icon {
+                cell.image = image
+            }
+        case kSBTitle:
+            string = item?.title
+        case kSBURL:
+            string = item?.URLString
+        case kSBDate:
+            let interval = item?.lastVisitedTimeInterval ?? 0
+            if interval > 0 {
+                let dateFormatter = NSDateFormatter()
+                dateFormatter.dateFormat = "%Y/%m/%d %H:%M:%S"
+                dateFormatter.formatterBehavior = .Behavior10_4
+                dateFormatter.dateStyle = .LongStyle
+                dateFormatter.timeStyle = .ShortStyle
+                dateFormatter.locale = NSLocale.currentLocale()
+                string = dateFormatter.stringFromDate(NSDate(timeIntervalSince1970: interval))
+            }
+        default:
+            break
+        }
+        if string != nil && !(string!.isEmpty) {
+            let attributes = [NSFontAttributeName: NSFont.systemFontOfSize(14.0),
+                              NSForegroundColorAttributeName: NSColor.whiteColor()]
+            let attributedString = NSAttributedString(string: string!, attributes: attributes)
+            cell.attributedStringValue = attributedString
+        }
+    }
+    
+    // MARK: Delegate
+    
+    override func controlTextDidChange(notification: NSNotification) {
+        if notification.object === searchField {
+            if searchField.stringValue.isEmpty {
+                showAllItems()
+            }
+        }
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        removeButton.enabled = tableView.selectedRowIndexes.count > 0
+    }
+    
+    // MARK: Construction
+    
+    func makeResponderChain() {
+        removeButton.nextKeyView = removeAllButton
+        backButton.nextKeyView = removeButton
+        tableView.nextKeyView = backButton
+        removeAllButton.nextKeyView = tableView
+    }
+    
+    // MARK: Actions
+    
+    func search(sender: AnyObject) {
+        let string = searchField.stringValue
+        if !string.isEmpty {
+            updateItems()
+            tableView.reloadData()
+        }
+    }
+    
+    func remove() {
+        let indexes = tableView.selectedRowIndexes
+        let removedItems = indexes.count > 0 ? items.objectsAtIndexes(indexes) : []
+        if !removedItems.isEmpty {
+            SBHistory.sharedHistory.removeItems(removedItems)
+            tableView.deselectAll(nil)
+            updateItems()
+            tableView.reloadData()
+        }
+    }
+    
+    func removeAll() {
+        let alert = NSAlert()
+        alert.messageText = NSLocalizedString("Are you sure you want to remove all items?", comment: "")
+        alert.addButtonWithTitle(NSLocalizedString("Remove All", comment: ""))
+        alert.addButtonWithTitle(NSLocalizedString("Cancel", comment: ""))
+        if alert.runModal() == NSOKButton {
+            SBHistory.sharedHistory.removeAllItems()
+            tableView.deselectAll(nil)
+            updateItems()
+            tableView.reloadData()
+        }
+    }
+    
+    func open() {
+        var urls: [NSURL] = []
+        let indexes = tableView.selectedRowIndexes
+        for var index = indexes.lastIndex; index != NSNotFound; index = indexes.indexLessThanIndex(index) {
+            let item: WebHistoryItem? = (index < items.count) ? items[index] : nil
+            let URLString = item?.URLString
+            let url: NSURL? = URLString != nil ? NSURL(string: URLString!) : nil
+            if url != nil {
+                urls.append(url!)
+            }
+        }
+        if target != nil && doneSelector != nil {
+            if target.respondsToSelector(doneSelector) {
+                SBPerform(target, doneSelector, urls as NSArray)
+            }
+        }
+    }
+}
